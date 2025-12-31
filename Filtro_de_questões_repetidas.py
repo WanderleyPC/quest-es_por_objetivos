a62192c19454c88c5f1e4ddf8c6baf737bc3fa2c
Wanderley Barbosa <wj5110709@gmail.com>
2025-12-31 14:53:30 -0300

    filtro 100%funcional

--- Filtro_de_questões_repetidas.py
+++ Filtro_de_questões_repetidas.py
@@ -6,6 +6,7 @@ def filtro_n_repetição(questões_novas):
         import os
     
         """Recebe as novas questões recem criadas na forma de uma lista, une elas as questões anteriores, calcula a similiaridade entre elas e retorna apenas as questões que diferem o suficiente das já presente em uma lista"""
+        
         # Verifica se o banco de questões já existe ou se ainda deve ser criado
         if os.path.exists('banco_de_questões.json'):
             # Trás as questões antigas do banco de questões
@@ -16,9 +17,6 @@ def filtro_n_repetição(questões_novas):
             questões= questões_anteriores+questões_novas
 
         else:
-            # Cria um banco de questões para quando não tem um 
-            with open("banco_de_questões.json", "w", encoding="utf-8") as arquivo:
-                json.dump(questões_novas, arquivo, ensure_ascii=False)
             # Considera apenas as questões novas como questões validas
             questões=questões_novas
             
@@ -35,17 +33,17 @@ def filtro_n_repetição(questões_novas):
         # Limiar de similaridade
         limiar = 0.6
 
-        # Vai indetificar quais são as novas questões que são repetidas
+        # Vai identificar quais são as novas questões que são repetidas
         novas_questões_são_repetidas=[]
         parecidas=0
 
         for i in range(len(questões)):
             for j in range(i + 1, len(questões)):
                 if similaridade[i][j] >= limiar:
-                    # print(f"Questões {i+1} e {j+1} são parecidas ({similaridade[i][j]:.2f})")
+                    print(f"Questões {i+1} e {j+1} são parecidas ({similaridade[i][j]:.2f})")
                     parecidas=parecidas+1
 
-                    # Ele vai salvar a **posição** da novas questões que sã0 repetidas
+                    # Ele vai salvar a **posição** da novas questões que são repetidas
                     novas_questões_são_repetidas.append(j-len(questões_anteriores))
         
         # Remove elementos que estejam repetidos na minha lista: eles surgem pois as verses uma nova questão é igual a uma outra nova questão. No caso eu mando a posição das duas novas questões para ambas serem eliminadas.
@@ -58,6 +56,8 @@ def filtro_n_repetição(questões_novas):
         if parecidas==0:
             print('Todas as questões são diferentes entre si')
         '''
+        print(novas_questões_são_repetidas)
+
         # Elimino as questões repetidas
         questões_não_repetidas=[]
         for i, a in enumerate(questões_novas):
@@ -68,3 +68,15 @@ def filtro_n_repetição(questões_novas):
    
     except Exception  as e:
         print(f"Ocorreu um erro na Filtro_de_questões_repetidas.py. Erro: {e}")
+
+""" # Mecanismo para testar o filtro de questões
+
+import json
+with open('teste.json', 'r', encoding='utf-8') as configuração:
+    aaa=json.load(configuração)
+
+filtro_n_repetição(aaa)
+for i in filtro_n_repetição(aaa):
+    print(i)
+
+"""
